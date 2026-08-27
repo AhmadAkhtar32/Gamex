11005a34dc15fd99da15cb4af022918093eb074b
AhmadAkhtar <ahmadrao3226@gmail.com>
2026-08-28 00:12:57 +0500

    commit

---
+++ src/app/admin/actions.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { destroyAdminSession } from "@/lib/admin-auth";
+
+export async function logoutAdmin() {
+  await destroyAdminSession();
+
+  redirect("/admin/login");
+}
