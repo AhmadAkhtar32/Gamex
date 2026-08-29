d41cb2749a439e38b238e8ca6449b0302fbfd69e
AhmadAkhtar <ahmadrao3226@gmail.com>
2026-08-30 02:16:50 +0500

    Commit

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import {
+  ArrowRight,
   ArrowUpRight,
   CalendarDays,
   Clock,
@@ -102,230 +103,311 @@ export function Blog({
             ================================================= */}
 
         {posts.length > 0 ? (
-          <ScrollSkew
-            amount={2}
-            className="
-              mt-14
-              grid
-              gap-6
-              md:grid-cols-2
-              lg:grid-cols-4
-            "
-          >
-            {posts.map(
-              (post, index) => (
-                <BlurReveal
-                  key={post.id}
-                  delay={
-                    index * 0.08
-                  }
-                  className="h-full"
-                >
-                  <SpotlightCard
-                    className="
-                      group
-                      flex
-                      h-full
-                      flex-col
-                      rounded-2xl
-                      border
-                      border-brand/15
-                      bg-white
-                      shadow-[0_18px_55px_-38px_rgba(23,49,96,0.30)]
-                      transition-all
-                      duration-300
-                      hover:-translate-y-1
-                      hover:border-brand/30
-                    "
+          <>
+            <ScrollSkew
+              amount={2}
+              className="
+                mt-14
+                grid
+                gap-6
+                md:grid-cols-2
+                lg:grid-cols-4
+              "
+            >
+              {posts.map(
+                (
+                  post,
+                  index
+                ) => (
+                  <BlurReveal
+                    key={post.id}
+                    delay={
+                      index *
+                      0.08
+                    }
+                    className="h-full"
                   >
-                    {/* =======================================
-                        IMAGE
-                        ======================================= */}
-
-                    <div
+                    <SpotlightCard
                       className="
-                        relative
-                        aspect-[16/10]
-                        overflow-hidden
-                      "
-                    >
-                      {/* eslint-disable-next-line @next/next/no-img-element */}
-                      <img
-                        src={post.image}
-                        alt={post.title}
-                        loading="lazy"
-                        className="
-                          h-full
-                          w-full
-                          object-cover
-                          transition-transform
-                          duration-700
-                          ease-out
-                          group-hover:scale-110
-                        "
-                      />
-
-                      <div
-                        className="
-                          absolute
-                          inset-0
-                          bg-gradient-to-t
-                          from-white
-                          via-transparent
-                          to-transparent
-                        "
-                      />
-
-                      {/* CATEGORY */}
-
-                      <span
-                        className="
-                          absolute
-                          left-3
-                          top-3
-                          rounded-md
-                          bg-brand/90
-                          px-2.5
-                          py-1
-                          text-[11px]
-                          font-bold
-                          uppercase
-                          tracking-wider
-                          text-white
-                        "
-                      >
-                        {post.category}
-                      </span>
-                    </div>
-
-                    {/* =======================================
-                        CARD CONTENT
-                        ======================================= */}
-
-                    <div
-                      className="
+                        group
                         flex
-                        flex-1
+                        h-full
                         flex-col
-                        p-5
+                        rounded-2xl
+                        border
+                        border-brand/15
+                        bg-white
+                        shadow-[0_18px_55px_-38px_rgba(23,49,96,0.30)]
+                        transition-all
+                        duration-300
+                        hover:-translate-y-1
+                        hover:border-brand/30
                       "
                     >
-                      {/* DATE / READ TIME */}
-
-                      <div
-                        className="
-                          flex
-                          flex-wrap
-                          items-center
-                          gap-x-4
-                          gap-y-2
-                          text-xs
-                          uppercase
-                          tracking-wider
-                          text-slate-500
-                        "
-                      >
-                        <span
-                          className="
-                            inline-flex
-                            items-center
-                            gap-1
-                          "
-                        >
-                          <CalendarDays className="h-3.5 w-3.5" />
-
-                          {post.date}
-                        </span>
-
-                        <span
-                          className="
-                            inline-flex
-                            items-center
-                            gap-1
-                          "
-                        >
-                          <Clock className="h-3.5 w-3.5" />
-
-                          {post.readTime}
-                        </span>
-                      </div>
-
-                      {/* TITLE */}
-
-                      <h3
-                        className="
-                          mt-3
-                          font-display
-                          text-base
-                          font-bold
-                          leading-snug
-                          text-brand-deep
-                          transition-colors
-                          group-hover:text-brand
-                        "
-                      >
-                        {post.title}
-                      </h3>
-
-                      {/* EXCERPT */}
-
-                      <p
-                        className="
-                          mt-2
-                          text-sm
-                          leading-relaxed
-                          text-slate-600
-                        "
-                      >
-                        {post.excerpt}
-                      </p>
-
                       {/* =====================================
-                          ARTICLE LINK
+                          IMAGE
                           ===================================== */}
 
                       <Link
                         href={`/blog/${post.slug}`}
                         className="
-                          mt-auto
-                          inline-flex
-                          items-center
-                          gap-1.5
-                          pt-4
-                          text-sm
-                          font-bold
-                          uppercase
-                          tracking-wider
-                          text-brand
-                          transition-colors
-                          hover:text-brand-soft
+                          relative
+                          block
+                          aspect-[16/10]
+                          overflow-hidden
                         "
                       >
-                        {content.readMoreText}
-
-                        <ArrowUpRight
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img
+                          src={
+                            post.image
+                          }
+                          alt={
+                            post.title
+                          }
+                          loading="lazy"
                           className="
-                            h-4
-                            w-4
+                            h-full
+                            w-full
+                            object-cover
                             transition-transform
-                            duration-300
-                            group-hover:translate-x-0.5
-                            group-hover:-translate-y-0.5
+                            duration-700
+                            ease-out
+                            group-hover:scale-110
                           "
                         />
+
+                        <div
+                          className="
+                            absolute
+                            inset-0
+                            bg-gradient-to-t
+                            from-white
+                            via-transparent
+                            to-transparent
+                          "
+                        />
+
+                        {/* CATEGORY */}
+
+                        <span
+                          className="
+                            absolute
+                            left-3
+                            top-3
+                            rounded-md
+                            bg-brand/90
+                            px-2.5
+                            py-1
+                            text-[11px]
+                            font-bold
+                            uppercase
+                            tracking-wider
+                            text-white
+                          "
+                        >
+                          {
+                            post.category
+                          }
+                        </span>
                       </Link>
-                    </div>
-                  </SpotlightCard>
-                </BlurReveal>
-              )
-            )}
-          </ScrollSkew>
+
+                      {/* =====================================
+                          CARD CONTENT
+                          ===================================== */}
+
+                      <div
+                        className="
+                          flex
+                          flex-1
+                          flex-col
+                          p-5
+                        "
+                      >
+                        {/* DATE / READ TIME */}
+
+                        <div
+                          className="
+                            flex
+                            flex-wrap
+                            items-center
+                            gap-x-4
+                            gap-y-2
+                            text-xs
+                            uppercase
+                            tracking-wider
+                            text-slate-500
+                          "
+                        >
+                          <span
+                            className="
+                              inline-flex
+                              items-center
+                              gap-1
+                            "
+                          >
+                            <CalendarDays className="h-3.5 w-3.5" />
+
+                            {
+                              post.date
+                            }
+                          </span>
+
+                          <span
+                            className="
+                              inline-flex
+                              items-center
+                              gap-1
+                            "
+                          >
+                            <Clock className="h-3.5 w-3.5" />
+
+                            {
+                              post.readTime
+                            }
+                          </span>
+                        </div>
+
+                        {/* TITLE */}
+
+                        <Link
+                          href={`/blog/${post.slug}`}
+                          className="block"
+                        >
+                          <h3
+                            className="
+                              mt-3
+                              font-display
+                              text-base
+                              font-bold
+                              leading-snug
+                              text-brand-deep
+                              transition-colors
+                              group-hover:text-brand
+                            "
+                          >
+                            {
+                              post.title
+                            }
+                          </h3>
+                        </Link>
+
+                        {/* EXCERPT */}
+
+                        <p
+                          className="
+                            mt-2
+                            text-sm
+                            leading-relaxed
+                            text-slate-600
+                          "
+                        >
+                          {
+                            post.excerpt
+                          }
+                        </p>
+
+                        {/* ===================================
+                            ARTICLE LINK
+                            =================================== */}
+
+                        <Link
+                          href={`/blog/${post.slug}`}
+                          className="
+                            mt-auto
+                            inline-flex
+                            items-center
+                            gap-1.5
+                            pt-4
+                            text-sm
+                            font-bold
+                            uppercase
+                            tracking-wider
+                            text-brand
+                            transition-colors
+                            hover:text-brand-soft
+                          "
+                        >
+                          {
+                            content.readMoreText
+                          }
+
+                          <ArrowUpRight
+                            className="
+                              h-4
+                              w-4
+                              transition-transform
+                              duration-300
+                              group-hover:translate-x-0.5
+                              group-hover:-translate-y-0.5
+                            "
+                          />
+                        </Link>
+                      </div>
+                    </SpotlightCard>
+                  </BlurReveal>
+                )
+              )}
+            </ScrollSkew>
+
+            {/* ===============================================
+                VIEW ALL ARTICLES
+                =============================================== */}
+
+            <BlurReveal
+              delay={0.15}
+              className="
+                mt-12
+                flex
+                justify-center
+              "
+            >
+              <Link
+                href="/blog"
+                className="
+                  group
+                  inline-flex
+                  items-center
+                  justify-center
+                  gap-3
+                  rounded-xl
+                  border
+                  border-brand/20
+                  bg-white
+                  px-6
+                  py-3.5
+                  font-display
+                  text-xs
+                  font-bold
+                  uppercase
+                  tracking-[0.15em]
+                  text-brand
+                  shadow-[0_15px_40px_-28px_rgba(23,49,96,0.35)]
+                  transition-all
+                  duration-300
+                  hover:-translate-y-0.5
+                  hover:border-brand
+                  hover:bg-brand
+                  hover:text-white
+                "
+              >
+                View All Articles
+
+                <ArrowRight
+                  className="
+                    h-4
+                    w-4
+                    transition-transform
+                    duration-300
+                    group-hover:translate-x-1
+                  "
+                />
+              </Link>
+            </BlurReveal>
+          </>
         ) : (
           /* =================================================
              NO POSTS
-
-             We keep the Blog heading visible because the admin
-             may intentionally have hidden/deleted every post.
              ================================================= */
 
           <div
